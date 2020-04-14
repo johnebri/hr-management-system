5f7e63ba3f3801895af60c82cdb6d59722bf25fc
johnebri <john.ebri@yahoo.com>
2020-04-14 18:05:00 +0100

    Setup, new employee

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+
+    public function showAll() {
+        $employees = \App\Employees::all();
+        return view('employee.showall', compact('employees'));
+    }
+
+    public function create() 
+    {
+        return view('employee.create');
+    }
+
+    public function store() 
+    {
+        $data = request()->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'gender' => 'required',
+            'date_of_birth' => 'required',
+            'date_of_employment' => 'required'
+        ]);
+
+        $employee = \App\Employees::create($data);
+
+        return redirect('home');
+    }
+}
+
